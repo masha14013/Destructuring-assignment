6bab6f64d7b3a3b6d9e54927154567ed08855c31
masha14013 <masha14013@rambler.ru>
2022-09-19 20:21:23 +0300

    added file test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+const HIGH_TEMPERATURES = {
+	yesterday: 75,
+	today: 77,
+	tomorrow: 80
+};
+
+// Destructuring assignment
+
+const { today } = HIGH_TEMPERATURES;
+const { tomorrow } = HIGH_TEMPERATURES;
